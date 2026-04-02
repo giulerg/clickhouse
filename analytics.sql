34c4820fd139a556556fcea8a4186aa626809bca
giulerg <henrytheanxious@gmail.com>
2026-04-02 11:45:10 +0300

    cohort

--- analytics.sql
+++ analytics.sql
@@ -29,6 +29,34 @@ SELECT
 FROM dau_cte
 LEFT JOIN mau_CTE ON 1=1;
 
+select * from bronze.raw_orders
 
+-- когортный анализ удержания
+WITH cohort_cte AS (
+    SELECT
+        user_id,
+        MIN(toStartOfMonth(r.paid_at)) AS first_pay
+    FROM bronze.raw_orders  r
+    WHERE user_id IS NOT NULL
+        AND paid_at IS NOT NULL
+        AND  order_status IN ('delivered','paid')
+    GROUP BY user_id
+), all_orders_cte AS (
+    SELECT
+        user_id,
+        toStartOfMonth(r.paid_at) AS  pay_date,
+        round(SUM(gross_amount), 2) AS amount
 
+    FROM bronze.raw_orders  r
+    WHERE user_id IS NOT NULL
+        AND paid_at IS NOT NULL
+        AND  order_status IN ('delivered','paid')
+    GROUP BY user_id, pay_date
+    )
+SELECT
+    a.user_id,
+    date_diff('month', first_pay, pay_date) AS month_number
 
+FROM all_orders_cte a
+JOIN cohort_cte c  ON a.user_id = c.user_id;
+WHERE month_number <=12
